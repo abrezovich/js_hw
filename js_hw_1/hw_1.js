4141fc5ad47ad74dde23e073a2819c70b50e6fff
abrezovich <abrezovich@gmail.com>
2020-10-30 11:28:36 +0200

    added 30.10.2020 11:28

--- js_hw_1/hw_1.js
+++ js_hw_1/hw_1.js
@@ -1,1 +1,33 @@
 'use strict';
+////*****1 */
+const name = 'Генератор защитного поля';
+let price = 1000;
+let message = `Выбран ${name}, цена за штуку ${price} кредитов`;
+console.log(message);
+/////****2 */
+const total = 100;
+let ordered = 130;
+
+if (total > ordered) { 
+    message = "Заказ оформлен,  с Вами свяжется менеджер";    
+} else{message = "На складе недостаточно товаров";
+    };
+console.log(message);    
+////****3 */
+const ADMIN_PASSWORD = 'jqueryismyjam';
+let input = prompt("Введите пароль!");
+switch (input) {
+    case ADMIN_PASSWORD:
+        message = "Добро пожадовать!";
+        alert(message);
+        break;
+    case null:
+        message = "Отменено пользователем!";   
+        alert(message);
+        break;
+    default:
+        message = "Доступ запрещен, неверный пароль!";   
+        alert(message);
+        break;
+};
+
